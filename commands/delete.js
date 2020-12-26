dbd8aeba226bb232a8592caedd60fab1488fa46c
SiderealArt <nelson22768384@gmail.com>
2020-12-26 19:54:25 +0800

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -9,7 +9,7 @@ module.exports = {
         })
         let fetched;
         do {
-            fetched = await channel.fetchMessages({ limit: args[1] });
+            fetched = await message.channel.fetchMessages({ limit: args[1] });
             message.channel.bulkDelete(fetched);
         }
         while (fetched.size >= 2);
